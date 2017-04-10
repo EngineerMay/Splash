f63da2f712e4a2c3b836b5686168ae28604e853a
mayankchauhan <mayankchauhan632@gmail.com>
2017-04-10 17:16:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mayankchauhan/splashactivity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mayankchauhan.splashactivity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLAST_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLAST_TIME_OUT);
+    }
+}
